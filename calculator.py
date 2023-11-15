a4823d0e7a30d0c3ec022b766cf90bc8eb050088
Uvi0513 <150932692+Uvi0513@users.noreply.github.com>
2023-11-15 20:11:39 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero"
+
+if __name__ == "__main__":
+    num1 = float(input("Enter the first number: "))
+    operator = input("Enter the operator (+, -, *, /): ")
+    num2 = float(input("Enter the second number: "))
+
+    if operator == "+":
+        result = add(num1, num2)
+    elif operator == "-":
+        result = subtract(num1, num2)
+    elif operator == "*":
+        result = multiply(num1, num2)
+    elif operator == "/":
+        result = divide(num1, num2)
+    else:
+        result = "Invalid operator"
+
+    print(f"Result: {result}")
